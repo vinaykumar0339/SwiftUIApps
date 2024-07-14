13a63c86280a7ffd553e7d3ed354e7e4bf8e456b
vinaykumar0339 <66993050+vinaykumar0339@users.noreply.github.com>
2024-07-14 11:21:55 +0530

    cŗeate weather app

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by vinay kumar on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
